bcfa9a03bf90b553264e2787b586c5a60c57da1b
Metz I <metz.imai@gmail.com>
2023-09-11 07:04:44 -0400

    for loops examples

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function showMessage() {
-    alert('i wrote this on 9-2-23 :)')
-}
+// for (initialValue; whileSomething; changeExpression)
 
-showMessage()
+// for (let i=1; i<=10; i++) {
+//     console.log(`we are on row number ${i}`)
+// }
+
+// another example
+// for (let i=50; i>=0; i-=10) {
+//     console.log('hello' + i)
+// }
+
+// this is an infinite loop ^
+
+// const animals = ['lions', 'tigers', 'bears']
+// 
+// for (let i=0; i<=animals.length; i++) {
+//     console.log(i, animals[i])
+// }
+
+// spelling something backwards
+const word = 'dessert'
+for (let i=word.length-1; i>=0; i--) {
+    reversedWord += word[i]
+}
+console.log(reversedWord)
