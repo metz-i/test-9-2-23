e561b6ac82af9f861df8b460a97578151168cb38
Metz I <metz.imai@gmail.com>
2023-09-12 06:52:25 -0400

    guessing game while loops example

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const target = Math.floor(Math.random * 10)
 let guess = Math.floor(Math.random * 10)
 // we will (prob) have multiple guesses - so we can set guess as a var
 
-while (guess !== target) {
-	console.log(`guess: ${guess} target: ${target}`)
-	guess = Math.floor(Math.random * 10)
-}
-console.log(`guess: ${guess} target: ${target}`)
-console.log(`you win!!`)
+// while (guess !== target) {
+// 	console.log(`guess: ${guess} target: ${target}`)
+// 	guess = Math.floor(Math.random * 10)
+// }
+// console.log(`guess: ${guess} target: ${target}`)
+// console.log(`you win!!`)
+
