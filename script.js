eee9b406fbb0948b74bedcbf792459208456f31b
Metz I <metz.imai@gmail.com>
2023-09-13 20:06:20 -0400

    for...of example - value iterable

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
-const movieReviews = {
-    'Arrival' : 9.5,
-    'Alien' : 9,
-    'Amelie' : 8,
-    'In Bruges' : 9
-}
+// const movieReviews = {
+//     'Arrival' : 9.5,
+//     'Alien' : 9,
+//     'Amelie' : 8,
+//     'In Bruges' : 9
+// }
 
+// for...of example - accessing the value 
 // printing the movie (key) + rating (value)
-for (let movie of Object.keys(movieReviews)) {
-    console.log(movie, movieReviews[movie])
+// for (let movie of Object.keys(movieReviews)) {
+   // console.log(movie, movieReviews[movie])
+//}
+
+
+// const ratings = Object.values(movieReviews)
+// let total = 0 // counter
+
+// for (let score of ratings) {
+//     total += score
+// }
+
+// console.log(total /= ratings.length) // avg
+
+// // for...in example
+
+// let kenJenningsEarnings = {
+//     dailyDouble : 50000,
+//     tournamentOfChampions : 2500,
+//     battleOfDecades : 1000
+// }
+
+// let total = 0
+// for (let prop in kenJenningsEarnings) {
+//     total += kenJenningsEarnings[prop]
+// }
+
+// function rollDie() {
+// 	num = Math.floor(Math.random() * 6) + 1 // getting a number 1-6
+// 	console.log(`rolled: ${num}`)
+// }
+
+// function throwDie(numRolls) {
+//     for (let i=0; i<numRolls; i++) { 
+//         // starting at roll 0, we will keep rolling till we reach the numRolls specified
+//         rollDie()
+//     }
+// }
+
+function sum (x,y) {
+    console.log(x+y)
 }
-
-
-const ratings = Object.values(movieReviews)
-let total = 0 // counter
-
-for (let score of ratings) {
-    total += score
-}
-
-console.log(total /= ratings.length) // avg
