41a0fe42f3360877a624b7f05a8d9a94fe6f576b
Metz I <128658219+metz-i@users.noreply.github.com>
2023-09-17 10:50:39 -0400

    map examples and for...of examples

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-const numbers = [20, 21, 22, 23, 24, 25, 26, 27 ]
+// map
 
-numbers.forEach(function (num) { // will run the function for each num in numbers 
-    console.log(num * 2)
+// const texts = ['rofl', 'lmao', 'smh', 'omg']
+// const caps = texts.map(function (t) {
+//     return t.toUpperCase()
+// })
+
+const numbers = [20, 21, 22, 23, 24, 25, 26, 27]
+const words = ['asap', 'byob', 'rsvp', 'diy']
+
+const doubles = numbers.map(function (num) {
+    return num * 2 // anonymous function inside map function parentheses (arg)
 })
 
-books = [
-    {
-    title: 'A Gentleman in Moscow',
-    authors: ['Amor Towles'],
-    rating: 4.36
-    },
-    {
-        title: 'Good Omens',
-        authors: 'F. Scott Fitzgerald',
-        rating: 4.32
-    }
-]
-// 
-// books.forEach(function(book)) {
-//     console.log(book.title.toUpperCase())
-// }
-// 
-// for (let book of books) {
-//     console.log(book.title.toUpperCase())
-// }
+const numDetail = numbers.map(function(n) {
+    return {
+        value: n,
+        isEven: n%2 === 0
+    }   
+})
+
+// below example accomplishes same thing as first example
+const doubles2 = [] // empty array
+for (let num of numbers) {
+    doubles2.push(num * 2) // adding(pushing) doubled #s from another array into the empty array
+}
+
+
+words.map(function(word) {
+    return word.toUpperCase().split('').join('.')})
