ff0747d3cdbed8d7b5e4c369f8b7e30b7fc44af2
Metz I <128658219+metz-i@users.noreply.github.com>
2023-09-21 18:54:21 -0400

    reduce method example - multiplication

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
-let movies = [
-    "Fantastic Mr. Fox",
-    "Mr. and Mrs. Smith",
-    "Mrs. Doubtfire"
-]
+var nums = [3,4,5,6,7]
 
-let movie = movies.find(movie => {
-    return movie.includes('Mrs.')
-}) // "Mr. and Mrs. Smith"
-
-let movie2 = movies.find(m => m.indexOf('Mrs') === 0)
-// finding movie within movies where first word is "Mrs" 
-// and adding it to array `movie2`
-// returns "Mrs. Doubtfire"
-
-
-// `every` function
-const words = ['dog', 'dig', 'log', 'bag', 'wag']
-
-words.every(word => {
-    return word.length === 3
-}) // tests whether every word meets condition that the length is 3 characters long
-// True
-
-words.every(word => word[0] === 'd') // False
-
-words.every(w => {
-    let last_letter = w[w.length - 1]
-    return last_letter === 'g'
-}) // checking if every w in words has 'g' as the last letter
-// True
+const product = nums.reduce((total, currentVal) => {
+    return total * currentVal
+})
