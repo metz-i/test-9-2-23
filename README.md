e9a8104be83e8014f577349d98f353238e4a1a8d
Metz I <128658219+metz-i@users.noreply.github.com>
2023-10-05 21:22:11 -0400

    modifying styles across multiple selected elements using  and document.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # INTRODUCTION
--innerText vs textContent
+-modifying styles across multiple selected elements using `forEach` and document.`querySelectorAll`
 
--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-function textContent(rootNode) {
-    if ('textContent' in document.createTextNode(''))
-      return rootNode.textContent;
-  
-    var childNodes = rootNode.childNodes,
-        len = childNodes.length,
-        result = '';
-    
-    for (var i = 0; i < len; i++) {
-      if (childNodes[i].nodeType === 3)
-        result += childNodes[i].nodeValue;
-      else if (childNodes[i].nodeType === 1) 
-        result += textContent(childNodes[i]);
-    }
-  
-    return result;
-  }
+const allLis = document.querySelectorAll('li');
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+
+allLis.forEach(li, i) => {
+    const color = colors[i];
+    console.log(el, color);
+}
