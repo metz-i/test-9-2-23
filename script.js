fd82858871fb7e224e62fab383dd42627b05fb12
Metz I <128658219+metz-i@users.noreply.github.com>
2023-10-16 22:41:04 -0400

    async function example

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let myPromise = new Promise(function(myResolve, myReject) {
-    setTimeout(function() { myResolve("I love You !!"); }, 3000);
-    });
-    
-    myPromise.then(function(value) {
-    document.getElementById("demo").innerHTML = value;
-    });
+async function greet() {
+    return 'hello';
+}
+
+greet().then((val) => {
+    console.log('promise resolved with: ', val);
+})
+
